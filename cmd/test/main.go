d8f210f711ad8cd9ce44ffe69dadfce15c0de680
chipman <takesxq77@gmail.com>
2025-07-09 13:15:05 +0300

    Create crud logic for user

--- cmd/test/main.go
+++ cmd/test/main.go
@@ -1,1 +1,15 @@
 package main
+
+
+import (
+	"log"
+	"net/http"
+	serverhttp "domofon/server/http"
+)
+
+
+func main() {
+	router := serverhttp.NewRouter()
+	log.Println("Server started at :8080")
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
